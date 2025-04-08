aee80b05b5274a50b8ca1cf6f5cd1216b549240e
ibrahima DIALLO <ibrahimadiallo@groupeisi.com>
2025-04-08 00:53:06 +0000

    Removing imports of JavaFaker

--- backend/src/main/java/com/bowe/meetstudent/controllers/auth/AuthController.java
+++ backend/src/main/java/com/bowe/meetstudent/controllers/auth/AuthController.java
@@ -1,15 +1,6 @@
 package com.bowe.meetstudent.controllers.auth;
 
-import com.bowe.meetstudent.models.LoginRequest;
-import com.bowe.meetstudent.models.LoginResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
