148f1b43a7f0a33d23792d696668ed19587cfd90
ibrahima DIALLO <ibrahimadiallo@groupeisi.com>
2025-04-16 00:46:27 +0000

    security:loading user from memory

--- backend/src/main/java/com/bowe/meetstudent/config/SecurityConfig.java
+++ backend/src/main/java/com/bowe/meetstudent/config/SecurityConfig.java
@@ -3,23 +3,20 @@ package com.bowe.meetstudent.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-//    private final JwtAuthenticationFilter filter;
-//    private final CustomUserDetailsService customUserDetailsService;
 
     /**
      * Configures the security filter chain for the application.
@@ -28,28 +25,49 @@ public class SecurityConfig {
      * @return the configured {@link SecurityFilterChain} instance
      * @throws Exception if an error occurs while configuring the security filter chain
      */
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//
+//        httpSecurity
+//                .cors(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .sessionManagement(session ->
+//                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .securityMatcher("/**") // make this config for all my app. I could set it to work on the API only by doing this /api/**
+//                .authorizeHttpRequests( registry ->
+//                        registry
+//                                .requestMatchers("/").permitAll() // permit everyone to access the root path which means the welcome page here
+//                                .requestMatchers("/api/auth/**").permitAll() // the authentication (login and register) path available to enable authentication for everyone
+//                                .requestMatchers("/api/users").permitAll()
+//                                .anyRequest().authenticated()
+//                );
+//        return httpSecurity.build();
+//    }
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        // do the customer filter before using the UsernamePasswordAuthFilter class
-//        httpSecurity.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
-        httpSecurity
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .formLogin(AbstractHttpConfigurer::disable)
-                .securityMatcher("/**") // make this config for all my app. I could set it to work on the API only by doing this /api/**
-                .authorizeHttpRequests( registry ->
-                        registry
-                                .requestMatchers("/").permitAll() // permit everyone to access the root path which means the welcome page here
-                                .requestMatchers("/api/auth/**").permitAll() // the authentication (login and register) path available to enable authentication for everyone
-                                .requestMatchers("/api/users").permitAll()
-                                .anyRequest().authenticated()
-                );
-        return httpSecurity.build();
+        return http
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/").permitAll();
+                    registry.requestMatchers("/login").permitAll();
+                    registry.requestMatchers("/api/admin/**").hasRole("ADMIN");
+                    registry.anyRequest().authenticated();
+                })
+                .formLogin(formLogin -> formLogin.permitAll())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails normalUser = User.builder().username("ibra").password("$2a$12$vXmkPpdO.xfeQF8LyKZwfe6QgrTzu1SdjlNQtt5dIE5AlHPinai82").roles("USER").build();
+        UserDetails adminUser = User.builder().username("admin").password("$2a$12$mYPXyP/tq4d9GG2yc2Xu9eC5cXoF9HMt.98kOCqvKEOUOQJnrIy02").roles("ADMIN", "USER").build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+
     }
 
     /**
@@ -58,7 +76,7 @@ public class SecurityConfig {
      * @return a `PasswordEncoder` instance using `BCryptPasswordEncoder`
      */
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
